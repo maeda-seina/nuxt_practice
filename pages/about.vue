25f801ca0be91545e06522c3d438a83eaf1c72ab
seina.maeda <ayrton19601027@gmail.com>
2024-06-10 16:25:59 +0900

    npm init nuxt-app nuxt-practice & 「はじめる」（インストール〜おわりに）

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'About',
+})
+</script>
+
+<template>
+  <h1>About</h1>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+    <a href="https://v2.nuxt.com">External Link to another page</a>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
