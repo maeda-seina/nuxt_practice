97ba44a510aa3946896aff201b0bb6936a7e8195
seina.maeda <ayrton19601027@gmail.com>
2024-06-11 13:34:19 +0900

    機能（ファイルシステムルーティング） https://v2.nuxt.com/ja/docs/features/file-system-routing

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,5 +38,15 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  serverMiddleware: ['~/server-middleware/logger']
+  serverMiddleware: ['~/server-middleware/logger'],
+
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        name: 'custom',
+        path: '*',
+        component: resolve(__dirname, 'pages/404.vue')
+      })
+    }
+  }
 }
